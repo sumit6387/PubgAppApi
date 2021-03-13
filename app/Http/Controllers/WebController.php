5c7e35e7aeab92b5f5a1edafab793579ba978cae
sumit6387 <54669901+sumit6387@users.noreply.github.com>
2021-03-13 17:48:23 +0530

    update#31
    some changes

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Feedback;
+use App\Models\FeedBack;
 class WebController extends Controller
 {
     public function index(){
-        $data['feedbacks'] = Feedback::select(['feedback.*','users.profile_img as img'])->orderby('feedback.id' , 'desc')->join('users','feedback.user_id','=','users.id')->take(5)->get();
+        $data['feedbacks'] = FeedBack::select(['feedback.*','users.profile_img as img'])->orderby('feedback.id' , 'desc')->join('users','feedback.user_id','=','users.id')->take(5)->get();
         return view('index',$data);
     }
 }
